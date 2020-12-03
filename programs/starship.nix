b04c760a8003ac9865df2753b621e686fd0d5681
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-12-03 03:04:33 -0500

    [starship] Change prompt

--- programs/starship.nix
+++ programs/starship.nix
@@ -1,5 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  programs.starship.enable = true;
+  programs.starship = {
+    enable = true;
+    # config
+    settings = {
+      add_newline = false;
+      character = {
+        error_symbol = "[✗](bold red) ";
+        success_symbol = "[➜](bold green) ";
+      };
+    };
+  };
 }
