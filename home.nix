d2fb70b646a421daabc4ffbbb03cc99ebcab79c7
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-12-02 20:05:27 -0500

    [tmux] Set escapeTime and terminal

--- home.nix
+++ home.nix
@@ -114,7 +114,9 @@
     # config
     baseIndex = 1;
     disableConfirmationPrompt = true;
+    escapeTime = 0;
     keyMode = "vi";
     shortcut = "a";
+    terminal = "screen-256color";
   };
 }
