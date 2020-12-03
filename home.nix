beee8adba4c47a3d176ac7beef85cb6baad89905
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-12-02 20:44:04 -0500

    [neovim] Change CtrlP command to rg

--- home.nix
+++ home.nix
@@ -21,6 +21,7 @@
 
   home.packages = [
     pkgs.bat
+    pkgs.ripgrep
   ];
 
   programs.fish = {
@@ -88,8 +89,14 @@
       let mapleader = "\<Space>"
       let maplocalleader = ','
 
+      " airline
       let g:airline_powerline_fonts = 1
+      " ctrlp
+      set grepprg=rg\ --color=never
 
+      let g:ctrlp_use_caching = 0
+      let g:ctrlp_user_command = 'rg %s --files --color=never --glob ""'
+      " nerdtree
       let g:NERDTreeShowHidden = 1
 
       noremap <C-n> :NERDTreeToggle<CR>
