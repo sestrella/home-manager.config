9da3eaf7a008c58556282e29db9c8aded3e0e0c6
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-12-08 15:01:11 -0500

    [git] Set pull rebase to false

--- programs/git.nix
+++ programs/git.nix
@@ -4,6 +4,7 @@
   programs.git = {
     enable = true;
     # config
+    extraConfig.pull.rebase = false;
     userEmail = "2049686+sestrella@users.noreply.github.com";
     userName = "Sebastián Estrella";
   };
