95c14133a4297cc980ee3651e257b2139c6f3f56
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-12-09 02:58:24 -0500

    [tmux] Add some plugins

--- programs/tmux.nix
+++ programs/tmux.nix
@@ -1,12 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  programs.tmux = { enable = true;
+  programs.tmux = {
+    enable = true;
     # config
     baseIndex = 1;
     disableConfirmationPrompt = true;
     escapeTime = 0;
     keyMode = "vi";
+    plugins = with pkgs.tmuxPlugins; [
+      continuum
+      resurrect
+      sensible
+    ];
     shortcut = "a";
     terminal = "screen-256color";
   };
