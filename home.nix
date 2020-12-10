8cf2fd3e4b4191512b80f7c52ef6283db8aed75d
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-12-09 20:36:08 -0500

    Set stateVersion to 20.09

--- home.nix
+++ home.nix
@@ -27,7 +27,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "21.03";
+  home.stateVersion = "20.09";
 
   home.sessionVariables = {
     NIX_PATH = "$HOME/.nix-defexpr/channels:$NIX_PATH";
