e922841bfbc4e3d9398f41dc1bd8a940250c4370
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2021-01-18 22:30:25 -0500

    Refactor ultisnips configuration

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -58,9 +58,9 @@ in {
       {
         plugin = ultisnips;
         config = ''
-          let g:UltiSnipsExpandTrigger="<tab>"
-          let g:UltiSnipsJumpForwardTrigger="<c-b>"
-          let g:UltiSnipsJumpBackwardTrigger="<c-z>"
+          let g:UltiSnipsExpandTrigger= '<tab>'
+          let g:UltiSnipsJumpBackwardTrigger= '<c-z>'
+          let g:UltiSnipsJumpForwardTrigger= '<c-b>'
         '';
       }
       {
