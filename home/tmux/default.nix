1a167778034fa33e8a6ceb6352bd1b38a781645f
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2021-02-01 22:39:51 -0500

    [tmux] Install Nord

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -1,6 +1,18 @@
 { pkgs, ... }:
 
-{
+let
+  nord = pkgs.tmuxPlugins.mkDerivation {
+    pluginName = "nord-tmux";
+    rtpFilePath = "nord.tmux";
+    version = "2021-02-01";
+    src = pkgs.fetchFromGitHub {
+      owner = "arcticicestudio";
+      repo = "nord-tmux";
+      rev = "4e2dc2a5065f5e8e67366700f803c733682e8f8c";
+      sha256 = "0l97cqbnq31f769jak31ffb7bkf8rrg72w3vd0g3fjpq0717864a";
+    };
+  };
+in {
   programs.tmux = {
     enable = true;
     # config
@@ -9,6 +21,7 @@
     escapeTime = 0;
     keyMode = "vi";
     plugins = [
+      nord
       pkgs.tmuxPlugins.prefix-highlight
       pkgs.tmuxPlugins.resurrect
       pkgs.tmuxPlugins.sensible
