b6b46992341dbe89804b9a0d7da4094747e5519d
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2021-04-28 23:47:59 -0500

    Rename shell

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   sources = import ./nix/sources.nix {};
   pkgs = import sources.nixpkgs {};
 in pkgs.mkShell {
-  name = "nixpkgs";
+  name = "nix-home";
   buildInputs = [
     (import sources.home-manager { inherit pkgs; }).home-manager
     (import sources.niv {}).niv
