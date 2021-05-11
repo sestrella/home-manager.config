5645420daeb4f24d86d1eacda6308024a952a10a
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2021-05-11 11:57:22 -0500

    [niv] Instal from source

--- shell.nix
+++ shell.nix
@@ -5,7 +5,6 @@ in pkgs.mkShell {
   name = "nix-home";
   buildInputs = [
     (import sources.home-manager { inherit pkgs; }).home-manager
-    pkgs.niv
   ];
   shellHook = ''
     export NIX_PATH="nixpkgs=${sources.nixpkgs}"
