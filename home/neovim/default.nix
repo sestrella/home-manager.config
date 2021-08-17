4be737b3065bdd77b6d18136162a862c2428e566
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2021-08-16 21:30:24 -0500

    Fix :checkhealth warnings

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -22,9 +22,10 @@
 
       set termguicolors
 
-      " let g:loaded_perl_provider=0
-      " let g:loaded_python_provider=0
-      " let g:loaded_ruby_provider=0
+      " INFO: Avoid issues running :checkhealth
+      let g:loaded_perl_provider=0
+      let g:loaded_python_provider=0
+      let g:loaded_ruby_provider=0
 
       let mapleader = "\<Space>"
       let maplocalleader = ','
