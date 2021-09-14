75de6a0aeeb1388f67282aebd0acd32bb2adf472
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2021-09-14 12:21:37 -0500

    [starship] Remove newline

--- home.nix
+++ home.nix
@@ -51,6 +51,7 @@ in {
   programs.starship = {
     enable = true;
     settings = {
+      add_newline = false;
       character = {
         success_symbol = "[➜](bold green) ";
         error_symbol = "[✗](bold red) ";
