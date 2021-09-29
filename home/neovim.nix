a0f6345338734410fd2109e56cb7910f75fee666
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2021-09-29 12:26:29 -0500

    [nvim] Setup lsp-colors

--- home/neovim.nix
+++ home/neovim.nix
@@ -118,6 +118,19 @@
           EOF
         '';
       }
+      {
+        plugin = lsp-colors-nvim;
+        config = ''
+          lua <<EOF
+            require("lsp-colors").setup({
+              Error = "#db4b4b",
+              Warning = "#e0af68",
+              Information = "#0db9d7",
+              Hint = "#10B981"
+            })
+          EOF
+        '';
+      }
       vim-better-whitespace
       vim-nix
       vim-rails
