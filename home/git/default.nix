8ded23b89345be161074e50d3586b49e17bcd7f2
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2021-12-02 11:48:28 -0500

    [git] Remove diff3

--- home/git/default.nix
+++ home/git/default.nix
@@ -5,7 +5,6 @@
     enable = true;
     extraConfig = {
       init.defaultBranch = "main";
-      merge.conflictstyle = "diff3";
       push.default = "current";
     };
     userEmail = "2049686+sestrella@users.noreply.github.com";
