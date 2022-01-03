0ceca17e8130efd886097910b652f1f6ed9c4599
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-01-02 22:27:06 -0500

    Minor refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -3,8 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs: {
