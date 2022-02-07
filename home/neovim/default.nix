5802fc7c786ad10c29dab09d2fd9e5bfccd16aea
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-02-06 21:58:49 -0500

    Remove main config.lua

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -43,7 +43,19 @@ in
 
       nnoremap <c-l> :nohlsearch<cr>
 
-      lua require('config')
+      lua << EOF
+      vim.o.number = true
+      -- screen
+      vim.o.colorcolumn = "80"
+      -- spaces
+      vim.o.expandtab = true
+      vim.o.shiftwidth = 2
+      vim.o.softtabstop = 2
+      vim.o.tabstop = 2
+      -- windows
+      vim.o.splitbelow = true
+      vim.o.splitright = true
+      EOF
     '';
     plugins = [
       {
