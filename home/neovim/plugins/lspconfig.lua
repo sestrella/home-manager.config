8124138beb4e079889e52000efad857d76b5d09c
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-02-07 01:26:42 -0500

    Reformat codes

--- home/neovim/plugins/lspconfig.lua
+++ home/neovim/plugins/lspconfig.lua
@@ -1,19 +1,18 @@
 local lspconfig = require('lspconfig');
 
+local servers = {'rnix', 'rust_analyzer', 'yamlls'};
 local opts = {noremap = true, silent = true};
 local keymaps = {
-  ['<leader>f'] = '<cmd>lua vim.lsp.buf.formatting()<CR>',
-  ['<leader>rn'] = '<cmd>lua vim.lsp.buf.rename()<CR>',
+    ['<leader>f'] = '<cmd>lua vim.lsp.buf.formatting()<CR>',
+    ['<leader>rn'] = '<cmd>lua vim.lsp.buf.rename()<CR>'
 };
 
 local on_attach = function(_, bufnr)
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc');
     for lhs, rhs in pairs(keymaps) do
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', lhs, rhs, opts);
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', lhs, rhs, opts);
     end
 end
-
-local servers = {'rnix', 'rust_analyzer', 'yamlls'};
 
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup({
