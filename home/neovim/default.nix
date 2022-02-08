7e8e846b299bd6bb956dcc622f4c295aeacb9508
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-02-08 00:33:02 -0500

    Revert some changes

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -6,10 +6,6 @@ let
     ${builtins.readFile source}
     EOF
   '';
-  mkPluginWithLuaConfig = name: {
-    plugin = pkgs.vimPlugins.${name};
-    config = readLuaFile "./plugins/${name}.lua";
-  };
 in
 {
   home.sessionVariables = {
@@ -24,6 +20,7 @@ in
 
   programs.neovim = {
     enable = true;
+    # config
     extraConfig = ''
       let mapleader = "\<Space>"
       let maplocalleader = ','
@@ -33,20 +30,50 @@ in
       ${readLuaFile ./init.lua}
     '';
     plugins = [
-      (mkPluginWithLuaConfig "NeoSolarized")
-      (mkPluginWithLuaConfig "haskell-vim")
-      (mkPluginWithLuaConfig "lualine-nvim")
-      (mkPluginWithLuaConfig "nvim-lspconfig")
-      (mkPluginWithLuaConfig "nvim-tree-lua")
-      (mkPluginWithLuaConfig "telescope-nvim")
+      {
+        plugin = pkgs.vimPlugins.haskell-vim;
+        config = readLuaFile ./plugins/haskell.lua;
+      }
+      {
+        plugin = pkgs.vimPlugins.NeoSolarized;
+        config = readLuaFile ./plugins/neosolarized.lua;
+      }
       pkgs.vimPlugins.lsp-colors-nvim
+      {
+        plugin = pkgs.vimPlugins.lualine-nvim;
+        config = readLuaFile ./plugins/lualine.lua;
+      }
+      {
+        plugin = pkgs.vimPlugins.nvim-lspconfig;
+        config = readLuaFile ./plugins/lspconfig.lua;
+      }
+      {
+        plugin = pkgs.vimPlugins.nvim-tree-lua;
+        config = readLuaFile ./plugins/tree.lua;
+      }
       pkgs.vimPlugins.nvim-web-devicons
       pkgs.vimPlugins.surround
+      {
+        plugin = pkgs.vimPlugins.telescope-nvim;
+        config = readLuaFile ./plugins/telescope.lua;
+      }
       pkgs.vimPlugins.vim-better-whitespace
       pkgs.vimPlugins.vim-nix
       pkgs.vimPlugins.vim-rails
       pkgs.vimPlugins.vim-terraform
       pkgs.vimPlugins.vim-toml
+      # Custom plugins
+      # TODO: Review LSP configuration
+      # plugins.cmp-buffer
+      # plugins.cmp-cmdline
+      # plugins.cmp-nvim-lsp
+      # plugins.cmp-path
+      # plugins.cmp-vsnip
+      # plugins.comment
+      # plugins.friendly-snippets
+      # plugins.lspkind-nvim
+      # plugins.nvim-cmp
+      # plugins.nvim-lspconfig
     ];
     viAlias = true;
     vimAlias = true;
