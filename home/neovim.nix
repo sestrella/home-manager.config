c8de66b107ef103d1bfb477a789dd3364dec95f4
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-07-20 00:04:58 -0500

    Change splits / Add colorcolumn and cursorline

--- home/neovim.nix
+++ home/neovim.nix
@@ -5,11 +5,15 @@
     enable = true;
     extraConfig = ''
       lua << EOF
+      vim.o.colorcolumn = "80"
+      vim.o.cursorline = true
       vim.o.expandtab = true
       vim.o.laststatus = 3
       vim.o.number = true
       vim.o.shiftwidth = 2
       vim.o.softtabstop = 2
+      vim.o.splitbelow = true
+      vim.o.splitright = true
       vim.o.tabstop = 2
       EOF
     '';
