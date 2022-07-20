fd49d908b7a4b7a5c4a77c18957f26a96e486875
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-07-20 00:01:11 -0500

    Replace tab with spaces

--- home/neovim.nix
+++ home/neovim.nix
@@ -4,7 +4,14 @@
   programs.neovim = {
     enable = true;
     extraConfig = ''
-      lua vim.o.number = true
+      lua << EOF
+      vim.o.expandtab = true
+      vim.o.laststatus = 3
+      vim.o.number = true
+      vim.o.shiftwidth = 2
+      vim.o.softtabstop = 2
+      vim.o.tabstop = 2
+      EOF
     '';
     plugins = with pkgs.vimPlugins; [
       {
