5515c7e7b5ab3f32f31ceeeb3b80db613304d33f
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-09-17 23:06:23 -0500

    Refactor neovim config / Set type to lua

--- home.nix
+++ home.nix
@@ -80,89 +80,86 @@
     };
   };
 
-  programs.neovim =
-    let
-      mkLuaConfig = config: ''
-        lua << EOF
-        ${config}
-        EOF
-      '';
-    in
-    {
-      enable = true;
-      extraConfig = mkLuaConfig ''
-        vim.o.colorcolumn = "80"
-        vim.o.cursorline = true
-        vim.o.expandtab = true
-        vim.o.ignorecase = true
-        vim.o.laststatus = 3
-        vim.o.number = true
-        vim.o.shiftwidth = 2
-        vim.o.softtabstop = 2
-        vim.o.splitbelow = true
-        vim.o.splitright = true
-        vim.o.tabstop = 2
-      '';
-      plugins = [
-        {
-          plugin = pkgs.vimPlugins.nvim-lspconfig;
-          config = mkLuaConfig ''
-            local on_attach = function(client, bufnr)
-              local bufopts = { noremap = true, silent = true, buffer = bufnr }
-              vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
-            end
+  programs.neovim = {
+    enable = true;
+    extraConfig = ''
+      lua << EOF
+      vim.o.colorcolumn = "80"
+      vim.o.cursorline = true
+      vim.o.expandtab = true
+      vim.o.ignorecase = true
+      vim.o.laststatus = 3
+      vim.o.number = true
+      vim.o.shiftwidth = 2
+      vim.o.softtabstop = 2
+      vim.o.splitbelow = true
+      vim.o.splitright = true
+      vim.o.tabstop = 2
+      EOF
+    '';
+    plugins = [
+      {
+        plugin = pkgs.vimPlugins.nvim-lspconfig;
+        config = ''
+          local on_attach = function(client, bufnr)
+            local bufopts = { noremap = true, silent = true, buffer = bufnr }
+            vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
+          end
 
-            local lspconfig = require("lspconfig")
+          local lspconfig = require("lspconfig")
 
-            lspconfig["rnix"].setup({
-              cmd = { "${pkgs.rnix-lsp}/bin/rnix-lsp" },
-              on_attach = on_attach
-            })
+          lspconfig["rnix"].setup({
+            cmd = { "${pkgs.rnix-lsp}/bin/rnix-lsp" },
+            on_attach = on_attach
+          })
 
-            lspconfig["terraformls"].setup({
-              cmd = { "${pkgs.terraform-ls}/bin/terraform-ls", "serve" },
-              on_attach = on_attach
-            })
+          lspconfig["terraformls"].setup({
+            cmd = { "${pkgs.terraform-ls}/bin/terraform-ls", "serve" },
+            on_attach = on_attach
+          })
 
-            lspconfig["yamlls"].setup({
-              cmd = { "${pkgs.yaml-language-server}/bin/yaml-language-server", "--stdio" },
-              on_attach = on_attach,
-              settings = {
-                yaml = {
-                  schemas = {
-                    ["https://json.schemastore.org/circleciconfig.json"] = "/.circleci/config.yml",
-                    ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*.yml"
-                  }
+          lspconfig["yamlls"].setup({
+            cmd = { "${pkgs.yaml-language-server}/bin/yaml-language-server", "--stdio" },
+            on_attach = on_attach,
+            settings = {
+              yaml = {
+                schemas = {
+                  ["https://json.schemastore.org/circleciconfig.json"] = "/.circleci/config.yml",
+                  ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*.yml"
                 }
               }
-            })
-          '';
-        }
-        {
-          plugin = pkgs.vimPlugins.nvim-treesitter;
-          config = mkLuaConfig ''
-            local parser_installed_dir = "~/.local/share/nvim-treesitter"
-            vim.opt.runtimepath:append(parser_installed_dir)
-            require("nvim-treesitter.configs").setup({
-              auto_install = true,
-              parser_install_dir = parser_installed_dir,
-              highlight = {
-                enable = true
-              }
-            })
-          '';
-        }
-        {
-          plugin = pkgs.vimPlugins.telescope-nvim;
-          config = mkLuaConfig ''
-            local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<c-p>", builtin.find_files, {})
-          '';
-        }
-      ];
-      viAlias = true;
-      vimAlias = true;
-    };
+            }
+          })
+        '';
+        type = "lua";
+      }
+      {
+        plugin = pkgs.vimPlugins.nvim-treesitter;
+        config = ''
+          local parser_installed_dir = "~/.local/share/nvim-treesitter"
+          vim.opt.runtimepath:append(parser_installed_dir)
+          require("nvim-treesitter.configs").setup({
+            auto_install = true,
+            parser_install_dir = parser_installed_dir,
+            highlight = {
+              enable = true
+            }
+          })
+        '';
+        type = "lua";
+      }
+      {
+        plugin = pkgs.vimPlugins.telescope-nvim;
+        config = ''
+          local builtin = require("telescope.builtin")
+          vim.keymap.set("n", "<c-p>", builtin.find_files, {})
+        '';
+        type = "lua";
+      }
+    ];
+    viAlias = true;
+    vimAlias = true;
+  };
 
   programs.starship.enable = true;
 
