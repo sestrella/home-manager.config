243a2be84a2abcb4d271e7d4463210f3c16da688
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-10-02 21:00:17 -0500

    Add flake-utils / Refactor code

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Home Manager configuration of Sebastian Estrella";
 
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -9,23 +10,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      homeConfigurations.sestrella = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+  outputs = { flake-utils, nixpkgs, home-manager, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.homeConfigurations.sestrella =
+          home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [
-          ./home.nix
-        ];
+            # Specify your home configuration modules here, for example,
+            # the path to your home.nix.
+            modules = [
+              ./home.nix
+            ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
-    };
+            # Optionally use extraSpecialArgs
+            # to pass through arguments to home.nix
+          };
+      }
+    );
 }
