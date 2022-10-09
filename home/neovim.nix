475baed26fb37492b82a191d1de6bea334c496ed
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-10-09 13:45:42 -0500

    Add hcl grammar and comment about deprecated lsp.buf.formatting command

--- home/neovim.nix
+++ home/neovim.nix
@@ -131,11 +131,13 @@ in
         '';
         type = "lua";
       }
+      #  Use vim.lsp.buf.format { async = true } instead
       {
         # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/vim.section.md#treesitter
         plugin = pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: [
           plugins.tree-sitter-dockerfile
           plugins.tree-sitter-haskell
+          plugins.tree-sitter-hcl
           plugins.tree-sitter-markdown
           plugins.tree-sitter-nix
           plugins.tree-sitter-rust
