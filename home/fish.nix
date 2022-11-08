8a5d03c84e30fbcee5a5a3ae535ad50a61b14e80
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-11-08 00:50:56 -0500

    Remove tmux abbrs from fish module

--- home/fish.nix
+++ home/fish.nix
@@ -7,12 +7,6 @@
       # home-manager
       hmo = "home-manager option";
       hms = "home-manager switch --flake .#sestrella";
-      # tmux - https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/tmux
-      ta = "tmux attach -t";
-      tkss = "tmux kill-session -t";
-      tksv = "tmux kill-server";
-      tl = "tmux list-sessions";
-      ts = "tmux new-session -s";
     };
     shellInit = ''
       if test -e /opt/homebrew/bin/brew
