649946f9245d81a5291870d5471a2f34e9645d95
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-11-07 21:17:35 -0500

    Add more key bindings for cmp and sources

--- home/neovim/cmp.nix
+++ home/neovim/cmp.nix
@@ -11,11 +11,16 @@
         end
       },
       mapping = cmp.mapping.preset.insert({
-        ["<Tab>"] = cmp.mapping.confirm({ select = true })
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true })
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "vsnip" }
+      }, {
+        { name = "buffer" }
       })
     })
   '';
