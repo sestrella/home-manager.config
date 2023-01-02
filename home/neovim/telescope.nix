b8c2903e15936726ebe956cec2af8862ab885ded
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-01-02 13:46:40 -0500

    [nvim] Show untracked files (git_files)

--- home/neovim/telescope.nix
+++ home/neovim/telescope.nix
@@ -3,8 +3,16 @@
 {
   plugin = pkgs.vimPlugins.telescope-nvim;
   config = ''
+    require('telescope').setup({
+      pickers = {
+        git_files = {
+          show_untracked = true
+        }
+      }
+    })
+
     local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<c-p>", builtin.git_files, {})
+    vim.keymap.set("n", "<c-p>", builtin.git_files, { desc = "Find files" })
   '';
   type = "lua";
 }
