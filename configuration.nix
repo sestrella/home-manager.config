49fb8da565d7258680ea07da5c91fb1016127097
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-02-04 22:15:34 -0500

    Remap Caps to Ctrl

--- configuration.nix
+++ configuration.nix
@@ -35,16 +35,22 @@
     ];
   };
 
-  nix.settings = {
-    substituters = [
-      "https://cache.iog.io"
-    ];
-    trusted-public-keys = [
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-    ];
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+    settings = {
+      substituters = [
+        "https://cache.iog.io"
+      ];
+      trusted-public-keys = [
+        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      ];
+    };
   };
 
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-  '';
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToControl = true;
+  };
 }
