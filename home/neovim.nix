8ae22994608eda8f884cad4e3200ac4e61215c45
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-02-23 22:44:07 -0500

    Pass shfmt path to null-ls

--- home/neovim.nix
+++ home/neovim.nix
@@ -25,9 +25,6 @@
       vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", {})
       vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", {})
     '';
-    extraPackages = [
-      pkgs.shfmt
-    ];
     plugins =
       let
         pluginsWithConfig = map
--- home/neovim/null-ls.nix
+++ home/neovim/null-ls.nix
@@ -4,10 +4,12 @@
   plugin = pkgs.vimPlugins.null-ls-nvim;
   config = ''
     local null_ls = require("null-ls")
-    
+
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.shfmt
+        null_ls.builtins.formatting.shfmt.with({
+          command = "${pkgs.shfmt}/bin/shfmt"
+        })
       }
     })
   '';
