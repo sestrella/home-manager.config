f913bd1ff59aaa07c261e433af86c58da84f7398
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-04-23 21:37:30 -0500

    Setup devenv

--- flake.nix
+++ flake.nix
@@ -4,34 +4,53 @@
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    devenv.url = "github:cachix/devenv";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
-      url = "github:stackbuilders/home-manager/add_delta_package_option";
-      # url = "path:/Users/sestrella/code/stackbuilders/home-manager";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, darwin, home-manager, nixpkgs }: {
-    darwinConfigurations =
-      let
-        mkDarwinSystem = system: darwin.lib.darwinSystem {
-          inherit system;
-          modules = [
-            ./configuration.nix
-            home-manager.darwinModules.home-manager
-            {
-              home-manager = {
-                backupFileExtension = "bak";
-                users.sestrella = import ./home.nix;
-              };
-            }
-          ];
+  outputs = { self, darwin, devenv, flake-utils, home-manager, nixpkgs } @ inputs:
+    {
+      darwinConfigurations =
+        let
+          mkDarwinSystem = system: darwin.lib.darwinSystem {
+            inherit system;
+            modules = [
+              ./configuration.nix
+              home-manager.darwinModules.home-manager
+              {
+                home-manager = {
+                  backupFileExtension = "bak";
+                  users.sestrella = import ./home.nix;
+                };
+              }
+            ];
+          };
+        in
+        {
+          "Administrators-MacBook-Pro" = mkDarwinSystem "aarch64-darwin";
+          "ghactions" = mkDarwinSystem "x86_64-darwin";
         };
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        "Administrators-MacBook-Pro" = mkDarwinSystem "aarch64-darwin";
-        "ghactions" = mkDarwinSystem "x86_64-darwin";
-      };
-  };
+        devShells.default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            ({ pkgs, ... }: {
+              packages = [
+                pkgs.gitleaks
+              ];
+
+              pre-commit.hooks.nixpkgs-fmt.enable = true;
+            })
+          ];
+        };
+      });
 }
