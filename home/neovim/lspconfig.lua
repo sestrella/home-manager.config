4205c28adcab633f2896d13f04dd82ea4fe91dd0
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-04-26 04:07:07 -0500

    [neovim] Add some additional key bindings

--- home/neovim/lspconfig.lua
+++ home/neovim/lspconfig.lua
@@ -49,6 +49,9 @@ for server, options in pairs(servers) do
 	}))
 end
 
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 	callback = function(ev)
@@ -57,7 +60,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local opts = { buffer = ev.buf }
 
 		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("n", "<space>f", function()
 			vim.lsp.buf.format({ async = true })
 		end, opts)
