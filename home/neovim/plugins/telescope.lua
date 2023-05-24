8ec5c33505f6199ca36ed45d2e12dbfc92593bf1
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-05-24 01:51:02 -0500

    [neovim] Add some additional keybindings for telescope

--- home/neovim/plugins/telescope.lua
+++ home/neovim/plugins/telescope.lua
@@ -8,3 +8,8 @@ require("telescope").setup({
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<c-p>", builtin.git_files, { desc = "Find files" })
+
+-- https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
