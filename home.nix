71f2d3458b464ab14386836d5e1b7672e9cce1ff
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2023-10-21 03:27:03 -0500

    Fix CI (#20)

--- home.nix
+++ home.nix
@@ -1,11 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "sestrella";
-  home.homeDirectory = "/Users/sestrella";
-
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
@@ -78,4 +73,5 @@
   # nixpkgs.overlays = [
   #   vim-plugins-overlays.default
   # ];
+  programs.home-manager.enable = true;
 }
