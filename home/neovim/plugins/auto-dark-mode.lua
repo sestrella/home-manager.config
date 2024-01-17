56d396d4fd61ccda89aaea5734429627c1a2995a
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-01-16 21:31:51 -0500

    [nvim] Refactor auto-dark-mode configuration

--- home/neovim/plugins/auto-dark-mode.lua
+++ home/neovim/plugins/auto-dark-mode.lua
@@ -1,14 +1,15 @@
 local auto_dark_mode = require("auto-dark-mode")
 
+local set_mode = function(mode)
+	return function()
+		vim.api.nvim_set_option("background", mode)
+		vim.cmd("colorscheme solarized")
+	end
+end
+
 auto_dark_mode.setup({
 	update_interval = 1000,
-	set_dark_mode = function()
-		vim.api.nvim_set_option("background", "dark")
-		vim.cmd("colorscheme solarized")
-	end,
-	set_light_mode = function()
-		vim.api.nvim_set_option("background", "light")
-		vim.cmd("colorscheme solarized")
-	end,
+	set_dark_mode = set_mode("dark"),
+	set_light_mode = set_mode("light"),
 })
 auto_dark_mode.init()
