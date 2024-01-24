4e8f694ad5a39ca6d80a79b17d7b0b76d2c2da8d
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-01-23 22:51:37 -0500

    Pull custom vim and tmux plugins from overlay

--- flake.nix
+++ flake.nix
@@ -2,13 +2,17 @@
   description = "My Home Manager configuration";
 
   inputs = {
+    auto-dark-mode-nvim.flake = false;
+    auto-dark-mode-nvim.url = "github:f-person/auto-dark-mode.nvim";
     devenv.url = "github:cachix/devenv";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    tmux-dark-notify.flake = false;
+    tmux-dark-notify.url = "github:erikw/tmux-dark-notify";
   };
 
-  outputs = { self, devenv, nixpkgs, home-manager }: {
+  outputs = inputs@{ self, devenv, nixpkgs, home-manager, ... }: {
     homeConfigurations =
       let
         mkHomeManagerConfig = { system, modules }:
@@ -16,7 +20,25 @@
             inherit modules;
             pkgs = import nixpkgs {
               inherit system;
-              overlays = [ devenv.overlays.default ];
+              # https://nixos.wiki/wiki/Overlays
+              overlays = [
+                devenv.overlays.default
+                (final: prev: {
+                  tmuxPlugins = prev.tmuxPlugins // {
+                    tmux-dark-notify = prev.tmuxPlugins.mkTmuxPlugin {
+                      pluginName = "tmux-dark-notify";
+                      rtpFilePath = "main.tmux";
+                      src = inputs.tmux-dark-notify;
+                    };
+                  };
+                  vimPlugins = prev.vimPlugins // {
+                    auto-dark-mode-nvim = prev.vimUtils.buildVimPlugin {
+                      name = "auto-dark-mode.nvim";
+                      src = inputs.auto-dark-mode-nvim;
+                    };
+                  };
+                })
+              ];
             };
           };
       in
