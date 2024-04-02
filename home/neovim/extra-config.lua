a79a6576a3b35e94a17a64cd2d42c83d0655ab4a
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-04-01 22:38:07 -0500

    Add shortcuts to move easily between windows

--- home/neovim/extra-config.lua
+++ home/neovim/extra-config.lua
@@ -17,6 +17,11 @@ vim.o.tabstop = 2
 vim.o.splitright = true
 vim.o.splitbelow = true
 
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
 -- line numbers
 vim.o.number = true
 vim.o.relativenumber = true
