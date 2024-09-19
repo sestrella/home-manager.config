30c0b5de5b75b5c5bc11193cef4f01111c19334c
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-09-19 00:05:23 -0500

    Add nix-community cache

--- home/nix/default.nix
+++ home/nix/default.nix
@@ -13,6 +13,7 @@
       caches = {
         "cache.nixos.org" = "6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
         "devenv.cachix.org" = "w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+        "nix-community.cachix.org" = "mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
         "nixpkgs-python.cachix.org" = "hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU=";
         "nixpkgs-ruby.cachix.org" = "vrcdi50fTolOxWCZZkw0jakOnUI1T19oYJ+PRYdK4SM=";
         "nixpkgs-terraform.cachix.org" = "8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw=";
