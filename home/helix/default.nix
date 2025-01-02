86e37ef0929c733599686c00e0011ec3893dfddd
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-01-02 04:22:34 -0500

    [helix] Add more language servers

--- home/helix/default.nix
+++ home/helix/default.nix
@@ -5,10 +5,14 @@
     enable = true;
     defaultEditor = true;
     extraPackages = [
+      pkgs.ansible-language-server
+      pkgs.bash-language-server
+      pkgs.docker-compose-language-service
       pkgs.golangci-lint-langserver
       pkgs.gopls
       pkgs.nil
       pkgs.terraform-ls
+      pkgs.yaml-language-server
     ];
     languages = {
       language = [
