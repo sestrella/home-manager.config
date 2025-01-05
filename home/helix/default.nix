f3337fb4a0dabb4466b45277f7e70ae29934f1b7
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-01-04 22:06:31 -0500

    Point home-manager back to main repo

--- home/helix/default.nix
+++ home/helix/default.nix
@@ -36,13 +36,31 @@
     };
   };
 
-  home.activation.helixTheme = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    mkdir -p ~/.config/helix/themes
-    style=$(/usr/bin/defaults read -g AppleInterfaceStyle 2> /dev/null || echo "Light")
-    if [ "$style" == "Dark" ]; then
-      ln -sf ${config.programs.helix.package}/lib/runtime/themes/solarized_dark.toml ~/.config/helix/themes/solarized.toml
-    else
-      ln -sf ${config.programs.helix.package}/lib/runtime/themes/solarized_light.toml ~/.config/helix/themes/solarized.toml
-    fi
-  '';
+  # TODO: Test this approach
+  launchd.agents.sync-helix-theme = {
+    enable = true;
+    config = {
+      ProgramArguments =
+        let
+          script = pkgs.writeScript "sync-helix-theme" ''
+            mkdir =p ~/.config/helix/themes
+            if [ "$1" == "dark" ]; then
+              ln -sf ${config.programs.helix.package}/lib/runtime/themes/solarized_dark.toml ~/.config/helix/themes/solarized.toml
+            else
+              ln -sf ${config.programs.helix.package}/lib/runtime/themes/solarized_light.toml ~/.config/helix/themes/solarized.toml
+            fi
+          '';
+        in
+        [
+          "/opt/homebrew/bin/dark-notify"
+          "-c"
+          "${script}/bin/sync-helix-theme"
+        ];
+      RunAtLoad = true;
+      KeepAlive = {
+        Crashed = true;
+        SuccessfulExit = false;
+      };
+    };
+  };
 }
