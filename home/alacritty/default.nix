041187bd8a6b29b807afb3809eb78157cab0c3c4
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-01-05 21:42:37 -0500

    [alacritty] Fix service that syncs theme with OS appearance

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -14,39 +14,31 @@
     };
   };
 
-  # TODO: Call the same script on the alacritty agent
-  home.activation.alacrittyTheme = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    theme() {
-      local style=$(/usr/bin/defaults read -g AppleInterfaceStyle 2> /dev/null || echo "Light")
-      if [ "$style" == "Dark" ]; then
-        echo "${pkgs.alacritty-theme}/solarized_dark.toml"
-      else
-        echo "${pkgs.alacritty-theme}/solarized_light.toml"
-      fi
-    }
-
-    mkdir -p ~/.config/alacritty
-    ln -sf "$(theme)" ~/.config/alacritty/theme.toml
-  '';
-
   # TODO: Install dark-notify via nix
-  launchd.agents.alacritty.enable = true;
-  launchd.agents.alacritty.config = {
-    ProgramArguments =
-      let
-        alacrittyTheme = pkgs.writeScriptBin "alacritty-theme" ''
-          if [ "$1" == "dark" ]; then
-            ln -sf "${pkgs.alacritty-theme}/solarized_dark.toml" ~/.config/alacritty/theme.toml
-          else
-            ln -sf "${pkgs.alacritty-theme}/solarized_light.toml" ~/.config/alacritty/theme.toml
-          fi
-          touch -h ~/.config/alacritty/alacritty.toml
-        '';
-      in
-      [
-        "/opt/homebrew/bin/dark-notify"
-        "-c"
-        "${alacrittyTheme}/bin/alacritty-theme"
-      ];
+  launchd.agents.alacritty-theme = {
+    enable = true;
+    config = {
+      ProgramArguments =
+        let
+          script = pkgs.writeScriptBin "alacritty-theme" ''
+            if [ "$1" == "dark" ]; then
+              ln -sf ${pkgs.alacritty-theme}/solarized_dark.toml ~/.config/alacritty/theme.toml
+            else
+              ln -sf ${pkgs.alacritty-theme}/solarized_light.toml ~/.config/alacritty/theme.toml
+            fi
+            touch -h ~/.config/alacritty/alacritty.toml
+          '';
+        in
+          [
+          "/opt/homebrew/bin/dark-notify"
+          "-c"
+          "${script}/bin/alacritty-theme"
+        ];
+      RunAtLoad = true;
+      KeepAlive = {
+        Crashed = true;
+        SuccessfulExit = false;
+      };
+    };
   };
 }
