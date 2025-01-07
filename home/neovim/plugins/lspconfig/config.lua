4e4042d01593f70a7b77c579c2f817ffaf22a50d
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-01-07 00:08:03 -0500

    [neovim] Update lspconfig

--- home/neovim/plugins/lspconfig/config.lua
+++ home/neovim/plugins/lspconfig/config.lua
@@ -61,22 +61,20 @@ end
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 	callback = function(event)
-		vim.bo[event.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+		-- vim.bo[event.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
 		local map = function(keys, f, desc)
 			vim.keymap.set("n", keys, f, { buffer = event.buf, desc = desc })
 		end
 
 		local builtin = require("telescope.builtin")
-		map("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
-		map("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
-		map("gr", builtin.lsp_references, "[G]oto [R]eferences")
 
-		map("<leader>D", builtin.lsp_type_definitions, "Type [D]efinition")
-		map("<leader>ds", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
+		map("gd", builtin.lsp_definitions, "Goto definition")
+		map("gi", builtin.lsp_implementations, "Goto implementation")
+		map("gr", builtin.lsp_references, "Goto references")
+		map("gy", builtin.lsp_type_definitions, "Goto type definition")
 
-		map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-		map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-		map("K", vim.lsp.buf.hover, "Hover Documentation")
+		map("<leader>a", vim.lsp.buf.code_action, "Perform code action")
+		map("<leader>r", vim.lsp.buf.rename, "Rename symbol")
 	end,
 })
