a46a0be1da54fdfe93e9129a6bc28e071413e7cf
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-02-18 00:03:33 -0500

    [neovim] Add LSP keybindings

--- home/neovim/extra-config.lua
+++ home/neovim/extra-config.lua
@@ -171,6 +171,26 @@ for server, config in pairs(servers) do
 	lspconfig[server].setup(config)
 end
 
+-- https://github.com/nvim-lua/kickstart.nvim
+vim.api.nvim_create_autocmd("LspAttach", {
+	-- TODO: rename group
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(event)
+		local map = function(keys, func, desc, mode)
+			mode = mode or "n"
+			vim.keymap.set(mode, keys, func, { buffer = event.buf, desc })
+		end
+
+		map("gd", builtin.lsp_definitions, "Goto definition")
+		map("gi", builtin.lsp_implementations, "Goto implementation")
+		map("gr", builtin.lsp_references, "Goto references")
+		map("gy", builtin.lsp_type_definitions, "Goto type definition")
+
+		map("<leader>a", vim.lsp.buf.code_action, "Perform code action", { "n", "x" })
+		map("<leader>r", vim.lsp.buf.rename, "Rename symbol")
+	end,
+})
+
 require("conform").setup({
 	formatters_by_ft = {
 		go = { "golines" },
