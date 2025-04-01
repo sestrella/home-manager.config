5fb3a8573114bc13b9e32bd54bfec62a3dc63c73
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-04-01 13:54:41 -0500

    [neovim] Setup copilot

--- home/neovim/extra-config.lua
+++ home/neovim/extra-config.lua
@@ -128,10 +128,10 @@ cmp.setup({
 		["<C-Space>"] = cmp.mapping.complete({}),
 	}),
 	sources = cmp.config.sources({
-		{ name = "copilot" },
-		{ name = "nvim_lsp" },
-		{ name = "nvim_lsp_signature_help" },
-		{ name = "vsnip" },
+		{ name = "copilot", group_index = 2 },
+		{ name = "nvim_lsp", group_index = 2 },
+		{ name = "nvim_lsp_signature_help", group_index = 2 },
+		{ name = "vsnip", group_index = 2 },
 	}, {
 		{ name = "buffer" },
 		{ name = "path" },
