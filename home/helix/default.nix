6241cd8578e5a4536392bb6cb66df2a4071136c7
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-09-15 12:29:44 -0500

    Revert "[helix] Remove configuration"
    This reverts commit defa9045cc4ee2ce8c222eaa1a1f962b40acc8f2.

---
+++ home/helix/default.nix
@@ -0,0 +1,70 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+
+{
+  programs.helix = {
+    enable = false;
+    defaultEditor = false;
+    extraPackages = [
+      pkgs.ansible-language-server
+      pkgs.bash-language-server
+      pkgs.docker-compose-language-service
+      pkgs.gopls
+      pkgs.nixd
+      pkgs.nixfmt-rfc-style
+      pkgs.terraform-ls
+      pkgs.yaml-language-server
+    ];
+    languages = {
+      language = [
+        {
+          name = "nix";
+          formatter = {
+            command = "nixfmt";
+          };
+        }
+      ];
+    };
+    settings = {
+      editor = {
+        cursor-shape.insert = "bar";
+        end-of-line-diagnostics = "hint";
+        file-picker.hidden = false;
+        inline-diagnostics.cursor-line = "warning";
+        line-number = "relative";
+        rulers = [ 80 ];
+      };
+      theme = "solarized";
+    };
+  };
+
+  launchd.agents.helix-theme = {
+    enable = true;
+    config = {
+      ProgramArguments =
+        let
+          script = pkgs.writeScriptBin "helix-theme" ''
+            mkdir -p ~/.config/helix/themes
+            if [ "$1" == "dark" ]; then
+              ln -sf ${config.programs.helix.package}/lib/runtime/themes/solarized_dark.toml ~/.config/helix/themes/solarized.toml
+            else
+              ln -sf ${config.programs.helix.package}/lib/runtime/themes/solarized_light.toml ~/.config/helix/themes/solarized.toml
+            fi
+          '';
+        in
+        [
+          "/opt/homebrew/bin/dark-notify"
+          "-c"
+          "${script}/bin/helix-theme"
+        ];
+      RunAtLoad = true;
+      KeepAlive = {
+        Crashed = true;
+        SuccessfulExit = false;
+      };
+    };
+  };
+}
