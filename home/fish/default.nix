92623808f2f03320e433ca15b1d87f30723d3f40
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-12-08 22:33:53 -0500

    feat(home): conditionally load brew shellenv
    Adds a check to ensure that the `brew` executable exists before attempting to load its shell environment. This prevents errors when Homebrew is not installed or its path is not yet configured.
    
    auto-commit-msg{Version:0.4.0-dev Model:gemini-2.5-flash-lite ResponseTime:1.174472208s ExecutionTime:1.192584584s}

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -7,7 +7,10 @@
     loginShellInit = ''
       # fish_add_path /usr/local/bin
       # https://github.com/Homebrew/brew/blob/master/Library/Homebrew/cmd/shellenv.sh
-      # eval (/opt/homebrew/bin/brew shellenv)
+      if test -x /opt/homebrew/bin/brew
+        eval (/opt/homebrew/bin/brew shellenv)
+      end
+
       . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
     '';
   };
