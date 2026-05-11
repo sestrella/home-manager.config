6517e2faf1427a1dffe53dd082db423e3266bb94
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2026-05-11 17:05:11 -0500

    Install devenv

--- flake.nix
+++ flake.nix
@@ -2,12 +2,17 @@
   description = "Home Manager configuration of sestrella";
 
   inputs = {
-    devenv.url = "github:cachix/devenv/v2.0";
+    devenv.url = "github:cachix/devenv/v2.1";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM=";
+    extra-substituters = "https://devenv.cachix.org https://cachix.cachix.org";
   };
 
   outputs =
