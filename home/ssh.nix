3815ec302d0e2b9776f556cc233089069338f258
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2026-05-12 10:23:57 -0500

    [git] Custom hostname to clone personal projects

--- home/ssh.nix
+++ home/ssh.nix
@@ -5,11 +5,20 @@
     enable = true;
 
     enableDefaultConfig = false;
-    matchBlocks."github.com" = {
-      addKeysToAgent = "yes";
-      extraOptions.UseKeychain = "yes";
-      identitiesOnly = true;
-      identityFile = "~/.ssh/id_ed25519";
+    matchBlocks = {
+      "github.com" = {
+        addKeysToAgent = "yes";
+        extraOptions.UseKeychain = "yes";
+        identitiesOnly = true;
+        identityFile = "~/.ssh/id_ed25519";
+      };
+      github-personal = {
+        addKeysToAgent = "yes";
+        extraOptions.UseKeychain = "yes";
+        hostname = "github.com";
+        identitiesOnly = true;
+        identityFile = "~/.ssh/personal.id_ed25519";
+      };
     };
   };
 }
