6d4069529c8c44387af990edeed8572d85fac006
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2026-05-19 22:30:13 -0500

    [helix] Extract theme sync script to a separate file for testing

---
+++ home/helix/default.nix
@@ -0,0 +1,73 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  helixThemeSync = pkgs.writeShellApplication {
+    name = "helix-theme-sync";
+    text = ./helix-theme-sync;
+  };
+in
+{
+  programs.helix = {
+    enable = true;
+
+    defaultEditor = true;
+    extraPackages = [
+      pkgs.bash-language-server
+      pkgs.docker-compose-language-service
+      pkgs.elixir-ls
+      pkgs.nixd
+      pkgs.nixfmt
+      pkgs.shellcheck
+      pkgs.terraform-ls
+      pkgs.vscode-json-languageserver
+      pkgs.yaml-language-server
+    ];
+    languages = {
+      language = [
+        {
+          name = "elixir";
+          auto-format = true;
+        }
+        {
+          name = "nix";
+          auto-format = true;
+        }
+      ];
+    };
+    settings = {
+      editor = {
+        cursor-shape.insert = "bar";
+        file-picker.hidden = false;
+        line-number = "relative";
+        mouse = false;
+        rulers = [ 80 ];
+      };
+      theme = "solarized";
+    };
+  };
+
+  # This has already been implemented in the master branch.
+  xdg.configFile."helix/config.toml".onChange = "/usr/bin/pkill -USR1 hx || true";
+
+  launchd.agents.helix-theme-sync = {
+    enable = true;
+
+    config = {
+      Program = lib.getExe helixThemeSync;
+      EnvironmentVariables.HELIX_RUNTIME_DIR = "${config.programs.helix.package}/lib/runtime";
+      ProcessType = "Background";
+      RunAtLoad = true;
+      KeepAlive = {
+        Crashed = true;
+        SuccessfulExit = false;
+      };
+      StandardOutPath = "${config.home.homeDirectory}/.local/state/helix-theme-sync/logs/out.log";
+      StandardErrPath = "${config.home.homeDirectory}/.local/state/helix-theme-sync/logs/err.log";
+    };
+  };
+}
