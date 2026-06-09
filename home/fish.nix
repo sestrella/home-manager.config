76b21cc8004fd5655908e7d787dba7d4bbd6aae7
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2026-06-09 09:21:29 -0500

    Remove /usr/local/bin comment from fish config

--- home/fish.nix
+++ home/fish.nix
@@ -5,7 +5,6 @@
     enable = true;
 
     loginShellInit = ''
-      # fish_add_path /usr/local/bin
       # https://github.com/Homebrew/brew/blob/master/Library/Homebrew/cmd/shellenv.sh
       if test -x /opt/homebrew/bin/brew
         eval (/opt/homebrew/bin/brew shellenv)
