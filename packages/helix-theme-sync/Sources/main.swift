e99151f1c7ff544e04a8a5302ae13e6fe8e20554
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2026-07-11 15:08:52 -0500

    Fix observer and print some debugging messages

--- packages/helix-theme-sync/Sources/main.swift
+++ packages/helix-theme-sync/Sources/main.swift
@@ -2,21 +2,89 @@
 // https://docs.swift.org/swift-book
 
 import Cocoa
+import Foundation
 
-DistributedNotificationCenter.default().addObserver(
-	forName: Notification.Name("AppleInterfaceThemeChangedNotification"),
-	object: nil,
-	queue: .main
-) { _ in
-	print("Appearance changed")
+class ThemeChangedObserver {
+	let center = DistributedNotificationCenter.default
+	let fileManager = FileManager.default
+
+	private var configDir: String
+	private var runtimeDir: String
+	private var theme: String
+
+	private var observer: NSObjectProtocol?
+
+	init(configDir: String, runtimeDir: String, theme: String) {
+		self.configDir = configDir
+		self.runtimeDir = runtimeDir
+		self.theme = theme
+	}
+
+	func add() {
+		observer = center.addObserver(
+			forName: Notification.Name("AppleInterfaceThemeChangedNotification"),
+			object: nil,
+			queue: .main
+		) { _ in
+			self.themeChanged()
+		}
+
+		NSWorkspace.shared.notificationCenter.addObserver(
+			forName: NSWorkspace.didWakeNotification,
+			object: nil,
+			queue: nil
+		) { _ in
+			self.themeChanged()
+		}
+	}
+
+	func remove() {
+		guard let foo = observer else {
+			return
+		}
+
+		center.removeObserver(foo)
+	}
+
+	private func themeChanged() {
+		let isDark = UserDefaults.standard.string(forKey: "AppleInterfaceStyle") == "Dark"
+
+		if isDark {
+			symlinkTheme(suffix: "dark")
+			return
+		}
+
+		symlinkTheme(suffix: "light")
+	}
+
+	private func symlinkTheme(suffix: String) {
+		do {
+			let themesDir = "\(configDir)/themes"
+			print("Creating directory \(themesDir)")
+			try fileManager.createDirectory(
+				atPath: themesDir,
+				withIntermediateDirectories: true
+			)
+
+			let symlinkPath = "\(runtimeDir)/themes/\(theme)_\(suffix).toml"
+			let themePath = "\(themesDir)/\(theme).toml"
+			print("Symlinking \(themePath) into \(symlinkPath)")
+			try fileManager.createSymbolicLink(
+				atPath: symlinkPath,
+				withDestinationPath: themePath
+			)
+		} catch {
+			// TODO: Handle errors
+			print(error)
+		}
+	}
 }
 
-NSWorkspace.shared.notificationCenter.addObserver(
-	forName: NSWorkspace.didWakeNotification,
-	object: nil,
-	queue: nil
-) { _ in
-	print("Wake up hook")
-}
-
+let observer = ThemeChangedObserver(
+	configDir: "~/.config/helix",
+	runtimeDir: "/nix/store/gk2kj3ngbdbmhmgphcmsff5m8i4xf874-helix-default-runtime",
+	theme: "solarized"
+)
+observer.add()
 NSApplication.shared.run()
+observer.remove()
