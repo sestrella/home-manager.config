105e5f11cc2b13a63c059277835d9cc2fa7a284e
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2026-08-06 19:48:37 -0500

    Flake/add nix config (#65)

--- flake.nix
+++ flake.nix
@@ -15,10 +15,14 @@
     extra-substituters = [
       "https://devenv.cachix.org"
       "https://herdr.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://sestrella.cachix.org"
     ];
     extra-trusted-public-keys = [
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "herdr.cachix.org-1:3nH7IStRsS0ASfdonA0DCRR2ZrSCeWitZ7Kwew0cR4I="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "sestrella.cachix.org-1:uf75o4yckcsAOFu6ldfPug/kTUMybvT0IY61sck2qnA="
     ];
   };
 
