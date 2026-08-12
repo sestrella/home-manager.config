2135d47c29b9d418691dce2d931fa49ce6fa1e10
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2026-08-11 21:57:39 -0500

    Add reference to real flake

--- templates/import-module/flake.nix
+++ templates/import-module/flake.nix
@@ -1,13 +1,13 @@
 {
-  description = "A minimal flake template that you can adapt to your own project";
+  description = "TODO";
 
   inputs = {
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # home-manager-config.url = "github:sestrella/home-manager.config";
     home-manager-config.url = "path:../../";
-    # home-manager-config.url = "path:/Users/sestrella/.config/home-manager";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
   };
 
