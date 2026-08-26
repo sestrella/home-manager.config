20cdfff10b5f23bf50cc46142d8fb7641380861c
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2026-08-25 22:29:05 -0500

    feat: Simplify default template

--- templates/default/home.nix
+++ templates/default/home.nix
@@ -1,7 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
   home.username = "runner";
+
+  home.packages = [ pkgs.terraform ];
 
   programs.git.settings = {
     user.email = "john.doe@acme.com";
